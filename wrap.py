c8c2b0e1ece8374c6edb0b958566e9923bd4b8a1
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2023-08-01 10:47:52 -0700

    remove wraps

--- wrap.py
+++ wrap.py
@@ -178,10 +178,8 @@ def create_py_bindings(all_functions : dict, output_path: str, dll_prefix: str):
 
 import os
 import ctypes as ct
-from functools import wraps
 
 def catch_errors(f):
-    @wraps(f)
     def wrapper(*args):
         errcode = ct.c_int(0)
         errmsg = ct.c_char_p(0)
